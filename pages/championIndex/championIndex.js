de4bcd0ae1faf7d1518ba0d75fd553f3db06c1ab
yangchaojie456 <yangchaojie456@126.com>
2018-11-27 20:03:35 +0800

    Update championIndex.js

--- pages/championIndex/championIndex.js
+++ pages/championIndex/championIndex.js
@@ -1,20 +1,74 @@
-// pages/championIndex/championIndex.js
+var champion = require('../champion.js')
+var championData = champion.data
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    searchValue:'',
+    chioce:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(championData)
+    this.setData({
+      championData: championData
+    })
+    
   },
+  championSearchConfirm(e){
+    console.log(e.detail.value)
+    var searchValue = e.detail.value
+    this.setData({
+      searchValue: searchValue
+    })
+    this.filterChampion(this.data.chioce, searchValue)
+  },
+  championSearch(e){
+    console.log(e)
+  },
+  championChange(e){
+    console.log(e.detail.value)
+    this.setData({
+      chioce: e.detail.value
+    })
+    this.filterChampion(e.detail.value,this.data.searchValue)
+  },
+  filterChampion(chioce,value) {
+    this.setData({
+      scrollTop: 0
+    })
+    var chioce = chioce
+    var obj = {}
+    for (var key in championData) {
 
+      var currentTags = championData[key].tags
+      if (currentTags.length >= chioce.length) {
+        var flag = true
+        for (var i = 0; i < chioce.length; i++) {
+          if (!currentTags.includes(chioce[i])) {
+            flag = false
+            break;
+          }
+        }
+        if (flag) {
+          var currentName = championData[key].name
+          var currentTitle = championData[key].title
+          if (currentName.includes(value) || currentTitle.includes(value)) {
+            obj[key] = championData[key]
+          }          
+        }
+      }
+    }
+    this.setData({
+      championData: obj
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
